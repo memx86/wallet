232328e0532315b09a88f3132c381d486c15c6c0
Yevhen Malyshko <memx86@gmail.com>
2022-05-10 14:05:00 +0300

    routes and toast container

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,26 @@
+import { Fragment } from "react";
+import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import Home from "pages/Home";
+import Registration from "pages/Registration";
+import Login from "pages/Login";
+import DiagramTab from "components/DiagramTab";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Fragment>
+      App
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Registration />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/diagram" element={<DiagramTab />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+      <ToastContainer hideProgressBar />
+    </Fragment>
+  );
 };
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import "modern-normalize";
 import "./index.scss";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
