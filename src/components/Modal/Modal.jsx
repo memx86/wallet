fdf5c57cb31e6d97f153cab9d63cca119efe5cec
Oleksii Persatyi <persatyi@ukr.net>
2022-05-17 17:51:29 +0300

    added animation on modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,8 +1,15 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import s from "./Modal.module.scss";
+import { isLogoutSelector } from "redux/session";
+import { useSelector } from "react-redux";
+import { CSSTransition } from "react-transition-group";
+import animation from "assets/animations/ModalAnimation.module.scss";
+import backdropAnimation from "assets/animations/ModalBackdropAnimation.module.scss";
 
 function Modal({ children, closeModal, modalClassName = "" }) {
+  const [startModal, setStartModal] = useState(false);
+  const isLogout = useSelector(isLogoutSelector);
   useEffect(() => {
     document.addEventListener("keydown", onEsc);
     return () => {
@@ -19,9 +26,26 @@ function Modal({ children, closeModal, modalClassName = "" }) {
   };
 
   return (
-    <div className={s.backdrop} onClick={onBackdropClick}>
-      <div className={modalClassName}>{children}</div>
-    </div>
+    <CSSTransition
+      in={isLogout}
+      timeout={100}
+      classNames={backdropAnimation}
+      unmountOnExit={true}
+      appear={true}
+      onEntered={() => setStartModal(true)}
+      onExited={() => setStartModal(false)}
+    >
+      <div className={s.backdrop} onClick={onBackdropClick}>
+        <CSSTransition
+          in={startModal}
+          timeout={250}
+          classNames={animation}
+          unmountOnExit={true}
+        >
+          <div className={modalClassName}>{children}</div>
+        </CSSTransition>
+      </div>
+    </CSSTransition>
   );
 }
 Modal.propTypes = {
